2b02a329e273d5dfb8fcc642be74eead2460c9a3
Sizertt <93645062+Sizertt@users.noreply.github.com>
2021-11-07 07:44:52 +0300

    CylinderVolume

---
+++ src/CyliderVolume.java
@@ -0,0 +1,16 @@
+public class CyliderVolume {
+    public static void main(String[] args) {
+
+
+    // Эти переменные иницилизируются константами
+    double radius = 4; // радиус цилиндра
+    double height = 5; // высота цилиндра
+
+    // Переменная Volume иницилизируется динамически
+    // Вычисленным значением выражения
+        double volume = 3.1416 * radius * radius * height;
+        System.out.println("Обьем цилиндра равен " + volume);
+
+    } // main(String[]) method
+} // CylinderVolume class
+
